b3e1c31640ca6a246e020ad060621f600cdaf497
adam <devsacti@gmail.com>
2022-05-30 09:58:54 +0900

    PLAYGROUND-39 [FIX] 주문완료화면 READ 기능수정

--- src/main/java/com/hmall/team04/controller/order/OrderController2.java
+++ src/main/java/com/hmall/team04/controller/order/OrderController2.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hmall.team04.dto.order.OrderCompleteDTO;
-import com.hmall.team04.dto.order.OrderDTO;
-import com.hmall.team04.service.cart.CartService;
 import com.hmall.team04.service.order.OrderService;
 
 import lombok.extern.log4j.Log4j;
@@ -30,15 +29,49 @@ public class OrderController2 {
 	@Autowired
 	private OrderService orderService;
 	
+	//@PreAuthorize("isAuthenticated()")
+	@RequestMapping(value = "/orderComplete", method = { RequestMethod.POST }, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HashMap<String, String> orderComplete(OrderCompleteDTO ordercompleteDTO,Principal principal,HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		// 앞선 주문결제화면에서 유효한 거래라면 화면에서 데이터베이스로, 주문테이블, 주문상세테이블, 결제테이블들에 행이 Create된 상태이다.
+		// 다만 여기서 문제는 '주문결제' 화면에서 데이터베이스로 단방향이라는 점이다. 중간에 븥잡을 곳은 컨트롤러간 임시저장소인 세션뿐이다.
+		// 시큐리티 Princial을 활용하여 user_id를 이용하여 '가장 최신의' 그리고 '1개의' prd_order_id를 데이터베이스에서 주문완료화면으로 끌어와 Read할 수 있다.
+		// 하지만, 매번 특정 사용자의 모든 주문내역을 살펴서 최근 1개를 뽑는 것보다, 세션에 방금 생성된 prd_order_id를 저장하고 이와 Principal에 저장된 user_id와 함께 사용하여 데이터를 찾는 게 효율적이다.
+		// 왜냐하면 prd_order_id는 pk 설정하면서 인덱스를 설정하기 때문이다.
+		
+		// 주문결제화면에서 주문완료화면으로 넘어갈 때, 세션에 prd_board_id와 user_id가 저장되어있다는 것을 전제한다.
+		ordercompleteDTO.setPrd_order_id("merchant_1653809215425");
+		ordercompleteDTO.setUser_id("1");
+		
+		// 컨트롤러 간 공용 임시저장소인 session 을 불러와, orderInfo라는 key에 orderList를 value로 저장
+		HttpSession session = req.getSession();
+		session.setAttribute("ordercompleteDTO", ordercompleteDTO);
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("orderSuccess", "True");
+
+		return map;
+	}
+	
 	@RequestMapping(value = "/orderComplete", method = RequestMethod.GET)
-	public String order(Principal principal, Model model) {
+	public String orderComplete(Principal principal,HttpServletRequest req, HttpServletResponse res, Model model) {
 		try {
-//			ArrayList<OrderDTO> ordercompleteList = orderService.getOrderCompleteList(principal.getName());
-			ArrayList<OrderCompleteDTO> ordercompleteList = orderService.getOrderCompleteList("1");
+//			HttpSession session = req.getSession();
+//			OrderCompleteDTO ordercompleteDTO = (OrderCompleteDTO) session.getAttribute("ordercompleteDTO");
+			// 우선 사용 완료했으므로 삭제하여 혹시모를 용량문제 해소
+//			session.removeAttribute("ordercompleteDTO");
+			
+			OrderCompleteDTO ordercompleteDTO = new OrderCompleteDTO();
+			ordercompleteDTO.setUser_id("1");
+			ordercompleteDTO.setPrd_order_id("merchant_1653809215425");
+			ordercompleteDTO.setPrd_pmt_id("StdpayVBNKINIpayTest20220529162715283069");
+			
+			ArrayList<OrderCompleteDTO> ordercompleteList = orderService.getOrderCompleteList(ordercompleteDTO);
 			log.info(ordercompleteList);
 
-			OrderCompleteDTO ordercompleteDTO = orderService.getPrdPayment("1");
-			log.info(ordercompleteDTO);
+			OrderCompleteDTO orderpay = orderService.getPrdPayment(ordercompleteDTO);
+			log.info(orderpay);
 			
 			// prd_order_id
 			String prd_order_id = ordercompleteList.get(0).getPrd_order_id();
@@ -59,7 +92,7 @@ public class OrderController2 {
 			model.addAttribute("address_dest", address_dest);
 			model.addAttribute("hp_no", hp_no);
 			// pmt_amount, insert 될 때 총합으로 들어갈 것이므로, 여기선 그냥 빼오면 끝
-			model.addAttribute("ordercompleteDTO", ordercompleteDTO);
+			model.addAttribute("orderpay", orderpay);
 			
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -68,4 +101,19 @@ public class OrderController2 {
 
 		return "order.orderComplete";
 	}
+	
+	@RequestMapping(value = "/orderPayComplete", method = { RequestMethod.POST }, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HashMap<String, String> orderPayComplete(OrderCompleteDTO ordercompleteDTO,Principal principal,HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// 아임포트에서 ngrok을 통해 현재 localhost:8090/team04/orderPayComplete로 아래와 같이 전송
+		//  curl -H "Content-Type: application/json" -X POST -d '{ "imp_uid": "imp_1234567890", "merchant_uid": "order_id_8237352", "status": "paid" }' { NotificationURL }
+		log.info(ordercompleteDTO);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("orderPayComplete", "True");
+
+		return map;
+	}
+	
+	
 }
--- src/main/java/com/hmall/team04/dao/order/OrderDAO.java
+++ src/main/java/com/hmall/team04/dao/order/OrderDAO.java
@@ -3,15 +3,12 @@ package com.hmall.team04.dao.order;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.hmall.team04.dto.cart.CartDTO;
-import com.hmall.team04.dto.category.CategoryDTO;
 import com.hmall.team04.dto.order.OrderCompleteDTO;
-import com.hmall.team04.dto.order.OrderDTO;
 
 public interface OrderDAO {
-	ArrayList<OrderCompleteDTO> getOrderCompleteList(String user_id) throws SQLException;
+	ArrayList<OrderCompleteDTO> getOrderCompleteList(OrderCompleteDTO ordercompleteDTO) throws SQLException;
 
-	OrderCompleteDTO getPrdPayment(String user_id) throws SQLException;
+	OrderCompleteDTO getPrdPayment(OrderCompleteDTO ordercompleteDTO) throws SQLException;
 
 
 }
